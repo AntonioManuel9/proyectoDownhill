a0a6431ac9ae706ef3d7161370aca650ad883dfb
AntonioManuel9 <antonio_manuel12@hotmail.es>
2020-02-23 11:55:21 +0100

    Mapa añadido

--- src/app/patrocinadores/patrocinadores.page.ts
+++ src/app/patrocinadores/patrocinadores.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { CallNumber } from '@ionic-native/call-number/ngx';
+import {Map,tileLayer,marker} from 'leaflet';
 
 @Component({
   selector: 'app-patrocinadores',
@@ -9,7 +10,21 @@ import { CallNumber } from '@ionic-native/call-number/ngx';
 })
 export class PatrocinadoresPage implements OnInit {
 
+  map:Map;
+  newMarker:any;
+  address:string[];
+
   constructor(private router: Router, private callNumber: CallNumber) { }
+
+  ionViewDidEnter(){
+    this.loadMap();
+  }
+  // The below function is added
+  loadMap(){this.map = new Map("mapId").setView([36.6772,-5.4461], 14);
+  tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  { attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY- SA</a>'})
+    .addTo(this.map); // This line is added to add the Tile Layer to our map
+  }
 
   ngOnInit() {
   }
